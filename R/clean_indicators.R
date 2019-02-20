9e5ba139c398dbac5bf49abb18c9ae094179952c
Aaron Chafetz <achafetz@usaid.gov>
2019-02-20 16:22:43 -0500

    adding missed files to previous commits

---
+++ R/clean_indicators.R
@@ -0,0 +1,39 @@
+#' Clean up indicators and disaggs for ease of use
+#'
+#' @param df data frame to adjust
+#'
+#' @export
+#' @importFrom magrittr %>%
+
+clean_indicators <- function(df){
+
+  suppressWarnings(
+    df <- df %>%
+      tidyr::separate(indicatorcode,
+                      c("indicator", "numeratordenom", "disaggregate", NA, "otherdisaggregate"),
+                      sep = "\\."))
+
+  #create modalities & rename HTS
+  df <- df %>%
+    dplyr::mutate(modality = dplyr::case_when(
+      indicator == "HTS_INDEX_COMM" ~ "IndexMod",
+      indicator == "HTS_INDEX_FAC" ~ "Index",
+      stringr::str_detect(indicator, "HTS_TST.") ~
+        stringr::str_remove(indicator, "HTS_TST_")),
+      indicator = ifelse(stringr::str_detect(indicator, "HTS_(TST|INDEX)."), "HTS_TST", indicator))
+
+  #result status
+  df <- df %>%
+    dplyr::mutate(resultstatus = dplyr::case_when(
+      otherdisaggregate %in% c("NewPos", "KnownPos", "Positive") ~ "Positive",
+      otherdisaggregate %in% c("NewNeg", "Negative") ~ "Negative"),
+      otherdisaggregate = ifelse(!stringr::str_detect(indicator, "STAT") &
+                                   otherdisaggregate %in% c("NewPos", "Positive",
+                                                            "NewNeg", "Negative"),
+                                 as.character(NA), otherdisaggregate))
+  #move targets to end
+  df <- df %>%
+    dplyr::select(-fy2020_targets, dplyr::everything())
+
+  return(df)
+}
---
+++ R/reshape_dp.R
@@ -0,0 +1,33 @@
+#' Reshape Data Pack Long
+#'
+#' @param df data frame to adjust
+#'
+#' @export
+#' @importFrom magrittr %>%
+
+
+reshape_dp <- function(df){
+
+  #identify all mechanism columns for reshaping
+  mechs <- df %>%
+    dplyr::select(dplyr::matches("^(1|2|3|4|5|6|7|8|9).")) %>%
+    names()
+
+  #identify all key meta data columns to keep
+  key_cols <- c("indicatorcode", "coarseage", "sex", "keypop")
+
+  #reshape
+  df <- df %>%
+    #keep only relevant columns
+    dplyr::select(key_cols, mechs) %>%
+    #reshape long, dropping NA cols
+    tidyr::gather(mechanismid, fy2020_target, mechs, na.rm = TRUE) %>%
+    #change values to double
+    dplyr::mutate(fy2020_targets = as.double(fy2020_target)) %>%
+    #aggregate up to mechanism/ind/age/sex/keypop level
+    dplyr::group_by_at(dplyr::vars(mechanismid, key_cols)) %>%
+    dplyr::summarise_at(dplyr::vars(fy2020_targets), sum, na.rm = TRUE) %>%
+    dplyr::ungroup()
+
+  return(df)
+}
