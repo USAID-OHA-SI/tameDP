54ab0d6fc8b1383f00c302a39a6a8c8b7caf396b
Joshua <jodavis@usaid.gov>
2019-02-27 12:47:23 -0500

    function to import xlsb

--- R/import_dp.R
+++ R/import_dp.R
@@ -8,25 +8,27 @@
 
 import_dp <- function(filepath){
 
-  #check if extension is okay
-  if(!is_xls(filepath))
-    stop("Format not accepted! Must convert Data Pack to .xls or .xlsx.")
-
   #check if file is found
   if(!is_file(filepath))
     stop("Cannot find file! Check file path.")
 
-  #check that sheet exists
-  if(!is_sheet(filepath))
-    stop("No sheet called 'SNU x IM' found.")
+  #check if extension is okay
+  if(!is_xls(filepath)){
+    df <- import_xlsb(filepath)
+    } else {
 
-  #import Data Pack and convert to lower
-  df <-
-    readxl::read_excel(filepath,
-                       sheet = "SNU x IM",
-                       skip = 4,
-                       col_types = "text") %>%
-    dplyr::rename_all(~tolower(.))
+    #check that sheet exists
+    if(!is_sheet(filepath))
+      stop("No sheet called 'SNU x IM' found.")
+
+    #import Data Pack and convert to lower
+    df <-
+      readxl::read_excel(filepath,
+                         sheet = "SNU x IM",
+                         skip = 4,
+                         col_types = "text") %>%
+      dplyr::rename_all(~tolower(.))
+  }
 
   return(df)
 }
--- R/import_xlsb.R
+++ R/import_xlsb.R
@@ -1,27 +1,21 @@
-##
-
-install.packages("RODBC")
-library(RODBC)
-library(tidyverse)
-
-
-#---------------------------------------------------------
-
 #' Import SNU x IM tabs for .xlsb workbooks
 #'
-#' @param filepath
+#' @param filepath file path to the Data Pack importing
 #'
 #'
 #' @export
 #'
 #' @importFrom magrittr %>%
-get_xlsb <- function(filepath) {
+import_xlsb <- function(filepath) {
 
 con <-  RODBC::odbcConnectExcel2007(filepath)
 
 df <- RODBC::sqlFetch(con, "SNU x IM") %>%
-  dplyr::slice(4:n()) %>%
-  dplyr::select(-F8, -F10) %>%
+  tibble::as_tibble() %>%
+  dplyr::slice(4:dplyr::n()) %>%
+  janitor::clean_names() %>%
+  dplyr::select(-dplyr::starts_with("na")) %>%
+  dplyr::rename_at(dplyr::vars(dplyr::starts_with("x")), ~ stringr::str_remove(.,"x")) %>%
   dplyr::mutate_all(~as.character(.))
 
 
@@ -29,7 +23,7 @@ colnames(df) <- df[1, ]
 df <- df[-1 ,]
 
 RODBC::odbcCloseAll()
-
+return(df)
 
 
 }
