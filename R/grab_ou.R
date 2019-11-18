d5e11a447c78c1f98459a4c5140c687ff253a181
Aaron Chafetz <achafetz@usaid.gov>
2019-11-18 11:33:11 -0500

    add importFrom and param description to grab_ou()

--- R/grab_ou.R
+++ R/grab_ou.R
@@ -3,15 +3,18 @@
 #' Function to pull the OU name from cell B20 in 'Home' and store as an df
 #' For use later when running `import_plhiv`
 #'
-#' @param filepath
+#' @param filepath file path to the Data Pack importing, must be .xls
+#'
+#' @importFrom magrittr %>%
 #'
 #' @export
+
 grab_ou <- function(filepath){
-ou <- readxl::read_excel(filepath,
-                         sheet = "Home",
-                         range = "B20",
-                         col_types = "text") %>%
-  names()
+  ou <- readxl::read_excel(filepath,
+                           sheet = "Home",
+                           range = "B20",
+                           col_types = "text") %>%
+    names()
 
 return(ou)
 
