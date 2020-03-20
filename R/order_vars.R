92b763dd3cca46073a3e34b693375db2190bf85e
Aaron Chafetz <achafetz@usaid.gov>
2020-03-20 17:25:04 -0400

    add error handling when not running map_names

--- R/order_vars.R
+++ R/order_vars.R
@@ -6,6 +6,15 @@
 order_vars <- function(df){
 
   #order variables
+  if(!"fundingagency" %in% names(df)){
+    df <- df %>%
+      dplyr::mutate(fundingagency = NA_character_,
+                    primepartner = NA_character_,
+                    mech_name = NA_character_)
+  }
+  setdiff(names(df), c("operatingunit", "psnu", "psnuuid",
+                       "fundingagency", "mech_code", "primepartner", "mech_name"))
+
   if("mech_code" %in% names(df)){
     df <- dplyr::select(df,
                         operatingunit, psnu, psnuuid,
