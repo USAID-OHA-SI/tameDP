d84fa4f69ea0d9afaed4466fa4fcde90170d4601
Aaron Chafetz <achafetz@usaid.gov>
2021-02-09 15:05:47 -0500

    use value as target rather than creating from datapacktarget * share

--- R/agg_dp.R
+++ R/agg_dp.R
@@ -11,17 +11,19 @@ agg_dp <- function(df, psnu_lvl = FALSE){
 
   if(psnu_lvl == FALSE){
     #create IM level targets
-    df <- dplyr::mutate(df, targets = round(datapacktarget * share, 0))
+    # df <- dplyr::mutate(df, targets = round(datapacktarget * share, 0))
+    df <- dplyr::mutate(df, targets = round(value, 0))
     key_cols <- c("mech_code", key_cols)
   } else {
     #create PSNU level targets
-    df <- dplyr::mutate(df, targets = round(datapacktarget, 0))
+    # df <- dplyr::mutate(df, targets = round(datapacktarget, 0))
+    df <- dplyr::mutate(df, targets = round(value, 0))
   }
 
     #aggregate up to psnu/[mechanism/]ind/age/sex/keypop level
     df <- df %>%
       dplyr::group_by(dplyr::across(dplyr::all_of(key_cols))) %>%
-      dplyr::summarise(dplyr::across(c(targets, value), sum, na.rm = TRUE)) %>%
+      dplyr::summarise(dplyr::across(c(targets), sum, na.rm = TRUE)) %>%
       dplyr::ungroup() %>%
       dplyr::filter(targets != 0)
 
