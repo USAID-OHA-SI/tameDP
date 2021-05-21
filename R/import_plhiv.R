851cbca3426d724b924490246432464da18a219f
Aaron Chafetz <achafetz@usaid.gov>
2021-05-21 13:43:16 -0400

    update to work with COP21 data pack and better align with NAT_SUBNAT dataset

--- R/import_plhiv.R
+++ R/import_plhiv.R
@@ -1,37 +1,41 @@
 #' Import PLHIV and current on ART from COP20 Datapacks
 #'
-#' @param filepath cop19 datapack in xlsx format
+#' @param filepath cop20 datapack in xlsx format
 #'
 #' @export
-#'
-#' @importFrom magrittr %>%
+
 import_plhiv <- function(filepath){
 
-  ou <- grab_ou(filepath)
+  ou <- grab_cntry(filepath)
 
   df <- readxl::read_excel(filepath,
-                           sheet = "Epi Cascade I",
+                           sheet = "Cascade",
                            skip = 13,
                            col_types = "text") %>%
     dplyr::rename_all(tolower)
 
   df <- df %>%
-    dplyr::select(psnu,
+    dplyr::select(snu1,
+                  psnu,
                   age,
                   sex,
-                  population = `pop_est.na.age/sex.t`,
-                  PLHIV = `plhiv.na.age/sex/hivstatus.t`,
-                  prevalence = `hiv_prev.na.age/sex/hivstatus.t`,
-                  current_ART = `tx_curr_subnat.n.age/sex/hivstatus.t`,
-                  vl_suppressed = `tx_curr_subnat.n.age/sex/hivstatus.t`) %>%
-    tidyr::gather(indicator, val, population:vl_suppressed) %>%
-    dplyr::mutate(val = as.numeric(val)) %>%
-    dplyr::filter(val != 0) %>%
-    tidyr::separate(psnu, c("psnu", "psnuuid"), sep = " \\[#SNU]") %>%
-    mutate(psnuuid = stringr::str_remove_all(psnuuid, "\\[|\\]"))
+                  pop_est.t_1,
+                  plhiv.t_1,
+                  hiv_prev.t_1,
+                  tx_curr_subnat.t_1,
+                  vl_suppressed.t_1) %>%
+    tidyr::gather(indicator, targets, pop_est.t_1:vl_suppressed.t_1) %>%
+    dplyr::mutate(targets = as.numeric(targets),
+                  fiscal_year == 2022,
+                  indicator = indicator %>%
+                    stringr::str_remove(".t_1") %>%
+                    toupper %>%
+                    dplyr::recode("VL_SUPPRESSED" = "VL_SUPPRESSION_SUBNAT")) %>%
+    dplyr::filter(targets != 0) %>%
+    split_psnu()
 
   df <- df %>%
-    dplyr::mutate(operatingunit = ou)
+    dplyr::mutate(countryname = ou)
 
   return(df)
 }
