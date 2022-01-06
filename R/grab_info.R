01de0149b4150c49e4e73709605e1b4a34a78db1
Aaron Chafetz <achafetz@usaid.gov>
2022-01-06 09:51:58 -0500

    feat: rename grab_cntry to grab_info to pull either country or year info

---
+++ R/grab_info.R
@@ -0,0 +1,37 @@
+#' Pull Information from Data Pack "Home" Tab
+#'
+#' This function extract information stored in the Data Pack Home tab to
+#' identify either the country or what the fiscal year is.
+#'
+#' @param filepath file path to the Data Pack importing, must be .xlsx
+#' @param type either "country" or "year"
+#'
+#' @export
+#' @examplesIf FALSE
+#' path <- "../Downloads/DataPack_Jupiter_20200218.xlsx"
+#' cntry <- grab_info(path, "country")
+#' fy <- grab_info(path, "year")
+
+grab_info <- function(filepath, type){
+
+  #identify which cell range to pull from
+  cell <- ifelse(type == "year", "B10", "B20")
+
+  #read in cell information
+  info <- readxl::read_excel(filepath,
+                           sheet = "Home",
+                           range = cell,
+                           col_types = "text") %>%
+    names()
+
+  #convert to FY from COP year in cell
+  if(type == "year"){
+    info <- info %>%
+      stringr::str_extract("(?<=COP)[:alnum:]{2}") %>%
+      paste0("20", .) %>%
+      as.numeric() + 1
+  }
+
+return(info)
+
+}
