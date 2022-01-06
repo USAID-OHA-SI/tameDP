0f68df00d6f9ab50a4bd194d678f85e1cbc74bc2
Aaron Chafetz <achafetz@usaid.gov>
2022-01-06 09:56:52 -0500

    feat: mimic function for importing plhiv, depricating import_plhiv

--- R/import_plhiv.R
+++ R/import_plhiv.R
@@ -1,41 +1,16 @@
-#' Import PLHIV and current on ART from COP20 Datapacks
+#' Import PLHIV and current on ART Datapacks
 #'
-#' @param filepath cop20 datapack in xlsx format
+#' @param filepath datapack file path in xlsx format
 #'
 #' @export
+#' @keywords internal
 
 import_plhiv <- function(filepath){
 
-  ou <- grab_cntry(filepath)
+  .Deprecated("tame_plhiv",
+              msg = "The import_plhiv function has been replaced by tame_plhiv.")
 
-  df <- readxl::read_excel(filepath,
-                           sheet = "Cascade",
-                           skip = 13,
-                           col_types = "text") %>%
-    dplyr::rename_all(tolower)
+  df_plhiv <- tame_plhiv(filepath)
 
-  df <- df %>%
-    dplyr::select(snu1,
-                  psnu,
-                  age,
-                  sex,
-                  pop_est.t_1,
-                  plhiv.t_1,
-                  hiv_prev.t_1,
-                  tx_curr_subnat.t_1,
-                  vl_suppressed.t_1) %>%
-    tidyr::gather(indicator, targets, pop_est.t_1:vl_suppressed.t_1) %>%
-    dplyr::mutate(targets = as.numeric(targets),
-                  fiscal_year = 2022,
-                  indicator = indicator %>%
-                    stringr::str_remove(".t_1") %>%
-                    toupper %>%
-                    dplyr::recode("VL_SUPPRESSED" = "VL_SUPPRESSION_SUBNAT")) %>%
-    dplyr::filter(targets != 0) %>%
-    split_psnu()
-
-  df <- df %>%
-    dplyr::mutate(countryname = ou)
-
-  return(df)
+  return(df_plhiv)
 }
---
+++ R/tame_plhiv.R
@@ -0,0 +1,35 @@
+#' Tidy PLHIV data from Data Pack
+#'
+#' tame_plhiv is a sister function to tame_dp, which readings in the PLHIV data
+#' from the Data Pack and munging in into a tidy data frame to make it more usable to
+#' interact with the data than the way it is stored in the Data Pack. **Given
+#' the changes to the Data Pack each year, the function only works for the
+#' current COP year, COP21.**
+#'
+#' @param filepath file path to the Data Pack importing, must be .xlsx
+#'
+#' @export
+#' @family primary
+#'
+#' @examplesIf FALSE
+#' #DP file path
+#'   path <- "../Downloads/DataPack_Jupiter_20200218.xlsx"
+#' #read in data pack
+#'   df_plhiv <- tame_plhiv(path)
+
+tame_plhiv <- function(filepath){
+
+  #import Data Pack and convert to lower
+  df_plhiv <- import_dp(filepath, tab = "Cascade")
+
+  #refine columns and reshape
+  df_plhiv <- reshape_plhiv(df_plhiv)
+
+  #identify country
+  cntry <- grab_cntry(filepath)
+
+  #apply country name
+  df_plhiv <- dplyr::mutate(df_plhiv, countryname = cntry, .before = 1)
+
+  return(df_plhiv)
+}
