0d45c6b078c83484f79201bed07310a153523ec2
Aaron Chafetz <achafetz@usaid.gov>
2022-01-11 10:32:28 -0500

    fix: migrate all reshapes into same file

--- R/reshape_psnuim.R
+++ R/reshape_psnuim.R
@@ -20,6 +20,40 @@ reshape_dp <- function(df){
 
 
 
+#' Reshape Data Pack Tab Long
+#'
+#' This function limits the columns from a target tab (non PSNUxIM) to extract
+#' data and reshapes it long, so that it is tidy and more usable. This function
+#' also splits out the PSNU uid from the PSNU column.
+#'
+#' @param df data frame from import_dp()
+#' @family reshape
+#' @export
+
+reshape_tab <- function(df){
+
+  #reshape long and remove blank rows
+  df <- df %>%
+    tidyr::pivot_longer(dplyr::matches("(t|t_1)$"),
+                        names_to = "indicator",
+                        values_to = "targets",
+                        values_drop_na = TRUE,
+                        values_transform = list(targets = as.numeric)) %>%
+    dplyr::filter(targets != 0)
+
+  #clean indicator
+  df <- df %>%
+    dplyr::mutate(indicator = indicator %>%
+                    stringr::str_remove("(.t_1|.t)") %>%
+                    toupper %>%
+                    dplyr::recode("VL_SUPPRESSED" = "VL_SUPPRESSION_SUBNAT"))
+
+  #extract PSNU UID from PSNU column
+  df <- split_psnu(df)
+
+  return(df)
+
+}
 
 
 
