490d6b8f84b917ca5a18cc630b5a6deba73deaea
Aaron Chafetz <achafetz@usaid.gov>
2022-01-11 17:43:37 -0500

    feat: include snu1

--- R/order_vars.R
+++ R/order_vars.R
@@ -7,10 +7,14 @@
 
 order_vars <- function(df){
 
+  #add snu1 as blank col if using PSNUxIM
+  if("snu1" %ni% names(df))
+    df <- dplyr::mutate(df, snu1 = NA_character_)
+
   #order variables
   if("mech_code" %in% names(df)){
     df <- dplyr::select(df,
-                        operatingunit, countryname, psnu, psnuuid,
+                        operatingunit, countryname, snu1, psnu, psnuuid,
                         fundingagency, mech_code, primepartner, mech_name,
                         fiscal_year,
                         dplyr::starts_with("indicator"), disagg,
@@ -18,7 +22,7 @@ order_vars <- function(df){
                         targets)
   } else {
     df <- dplyr::select(df,
-                        operatingunit, countryname, psnu, psnuuid,
+                        operatingunit, countryname, snu1, psnu, psnuuid,
                         fiscal_year,
                         dplyr::starts_with("indicator"), disagg,
                         numeratordenom, age, sex, modality, statushiv, otherdisaggregate,
