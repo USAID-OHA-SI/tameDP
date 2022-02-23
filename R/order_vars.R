1fc8e17b600a453ae649364888279149912804a6
Aaron Chafetz <achafetz@usaid.gov>
2022-02-23 09:50:45 -0500

    feat: ensure cumulative is in output and as the correct class

--- R/order_vars.R
+++ R/order_vars.R
@@ -11,6 +11,10 @@ order_vars <- function(df){
   if(!"snu1" %in% names(df))
     df <- dplyr::mutate(df, snu1 = NA_character_)
 
+  #add cumulative if it doesn't exist
+  if(!"cumulative" %in% names(df))
+    df <- dplyr::mutate(df, cumulative = NA_real_)
+
   #order variables
   if("mech_code" %in% names(df)){
     df <- dplyr::select(df,
@@ -19,14 +23,14 @@ order_vars <- function(df){
                         fiscal_year,
                         dplyr::starts_with("indicator"), standardizeddisaggregate,
                         numeratordenom, ageasentered, sex, modality, statushiv, otherdisaggregate,
-                        targets)
+                        cumulative, targets)
   } else {
     df <- dplyr::select(df,
                         operatingunit, countryname, snu1, psnu, psnuuid,
                         fiscal_year,
                         dplyr::starts_with("indicator"), standardizeddisaggregate,
                         numeratordenom, ageasentered, sex, modality, statushiv, otherdisaggregate,
-                        targets)
+                        cumulative, targets)
   }
 
   return(df)
