0ab1c019d8f0914d9cc50836509c1824139d73e0
Aaron Chafetz <achafetz@usaid.gov>
2023-03-17 13:35:20 -0400

    feat: update tests

--- tests/testthat/test-tameDP.R
+++ tests/testthat/test-tameDP.R
@@ -8,38 +8,19 @@ test_that("correct tabs are returned for import", {
                "TX_TB_PREV", "PP", "OVC", "GEND", "AGYW", "PrEP", "KP_MAT",
                "KP Validation", "PSNUxIM")
 
+  target_tabs <- dp_tabs[!dp_tabs %in% c("Home", "Summary", "Spectrum",
+                                         "Prioritization", "KP Validation",
+                                         "PSNUxIM")]
+
   expect_error(return_tab("wrong") %>%
                   intersect(dp_tabs))
 
   expect_equal(intersect(return_tab("PSNUxIM"), dp_tabs),
                "PSNUxIM")
-  expect_equal(intersect(return_tab("PLHIV"), dp_tabs),
-               "Cascade")
+  expect_equal(intersect(return_tab("SUBNAT"), dp_tabs),
+               target_tabs)
   expect_equal(intersect(return_tab("ALL"), dp_tabs),
-               c(#"Home",
-                 #"Summary",
-                 #"Spectrum",
-                 #"Prioritization",
-                 "Cascade",
-                 "PMTCT",
-                 "EID",
-                 "TB",
-                 "VMMC",
-                 "KP",
-                 "HTS",
-                 "CXCA",
-                 "HTS_RECENT",
-                 "TX_TB_PREV",
-                 "PP",
-                 "OVC",
-                 "GEND",
-                 "AGYW",
-                 "PrEP",
-                 "KP_MAT"
-                 #"KP Validation",
-                 #"PSNUxIM"
-                 )
-               )
+               target_tabs)
 
 })
 
